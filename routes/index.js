e14c10082474342cb53d9265b8c12703daf25165
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-12-11 21:16:01 +0100

    server

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import { Express } from 'express';
+import AppController from '../controllers/AppController';
+
+/**
+ * Injects routes with their handlers to the given Express application.
+ * @param {Express} api
+ */
+const injectRoutes = (api) => {
+  api.get('/status', AppController.getStatus);
+  api.get('/stats', AppController.getStats);
+};
+
+export default injectRoutes;
